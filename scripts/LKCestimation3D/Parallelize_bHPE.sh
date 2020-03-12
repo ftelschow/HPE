cae03e87060d13722e5e851526605f953ab85631
Fabian Telschow <ftelschow@ucsd.edu>
2020-03-12 13:45:32 +0100

    changed counter

--- scripts/LKCestimation3D/Parallelize_bHPE.sh
+++ scripts/LKCestimation3D/Parallelize_bHPE.sh
@@ -3,7 +3,7 @@
 ####  script for parallization of the bHPE
 ########################################################################
 # Loop over parallelisations
-for parallel_count in {1..10}
+for parallel_count in {11..20}
 do
     matlab -nodesktop -nosplash -r "Simulate_LKCestim3D_isotropic( 10 , 'bHPE', 'isotropic','num2str($parallel_count)' ); exit" &
 done
